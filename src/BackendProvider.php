08196e67ed9d2f687f4e63bdd4511fcd3b31f62c
Florian Steiner <fs@adiwidjaja.com>
2015-06-30 11:26:20 +0200

    Added multiple stuff.

--- src/BackendProvider.php
+++ src/BackendProvider.php
@@ -11,7 +11,8 @@ class BackendProvider extends ServiceProvider
 		$this->loadViewsFrom(__DIR__.'/flo5581/larabackend/Views', 'Backend');
 
 		$this->publishes([
-			__DIR__.'/Views' => base_path('resources/views/admin')
+			__DIR__.'/Views' => base_path('resources/views/admin'),
+			__DIR__.'/Views/css' => public_path('/css')
 		]);
 	}
 
--- src/Controllers/AdminController.php
+++ src/Controllers/AdminController.php
@@ -6,12 +6,27 @@ use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 
+/**
+ * AdminController for Backend
+ * 
+ * @package  Flo\Backend
+ */
 class AdminController extends BaseController
 {
 	use DispatchesJobs, ValidatesRequests;
 
+	/**
+	 * Actions displayed in the menu
+	 *
+	 * @var array
+	 */
 	public static $displayed_actions = [];
 
+	/**
+	 * Returns the view given
+	 *
+	 * @return view
+	 */
 	protected function view($path, $data = [])
 	{
 		return view($path, $data)->with('actions', static::$displayed_actions);
