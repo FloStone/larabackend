b808b20c8b93d14b0ecefb2dded125783413df01
Florian Steiner <fs@adiwidjaja.com>
2015-07-13 15:00:52 +0200

    Fixed delete_form.blade.php relations

--- src/Views/templates/delete_form.blade.php
+++ src/Views/templates/delete_form.blade.php
@@ -1,3 +1,7 @@
+@use(Illuminate\Support\Collection)
+@use(Illuminate\Database\Eloquent\Collection as EloquentCollection)
+@use(Illuminate\Database\Eloquent\Model)
+@use(Flo\Backend\Classes\Recombinator)
 @define($model = get_class($data['model']))
 <div class="form">
 <h2>Are you sure you want to delete this ?</h2>
@@ -6,7 +10,19 @@
 	<div class="formfield">
 		<label for="{{$column}}">{{$properties['label'] ?: $column}}:</label>
 		<br>
+		@if(isset($properties['relation']))
+		@if($data['model']->$column instanceof EloquentCollection or $data['model']->$column instanceof Collection)
+		<p>
+		@foreach($data['model']->$column as $item)
+		{{Recombinator::getFirstArrayEntry($item)}}<br>
+		@endforeach
+		</p>
+		@else
+		<p>{{Recombinator::getFirstArrayEntry($data['model']->$column)}}</p>
+		@endif
+		@else
 		<p>{{$data['model']->$column}}</p>
+		@endif
 	</div>
 	@endforeach
 	<input type="hidden" name="_token" value="{{ csrf_token() }}">
