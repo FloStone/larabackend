8d212787b9020b86c72566f885215d1020255998
Florian Steiner <fs@adiwidjaja.com>
2015-07-15 15:22:45 +0200

    Added tostring method, added info method, updated version.

--- src/Controllers/AdminController.php
+++ src/Controllers/AdminController.php
@@ -40,7 +40,7 @@ abstract class AdminController extends BaseController implements AdminInterface
 {
 	use DispatchesJobs, ValidatesRequests;
 
-	const VERSION = '1.0.1';
+	const VERSION = '1.0.2';
 
 	/**
 	 * Actions displayed in the menu
@@ -268,5 +268,25 @@ abstract class AdminController extends BaseController implements AdminInterface
 		Auth::logout();
 
 		return redirect('/');
+	}
+
+	/**
+	 * Print out version of AdminController
+	 *
+	 * @return string
+	 */
+	public function __tostring()
+	{
+		return 'Laravel Backend Package Version '.static::VERSION;
+	}
+
+	/**
+	 * Get info about Backend Package
+	 *
+	 * @return string
+	 */
+	public function info()
+	{
+		echo $this;
 	}
 }
