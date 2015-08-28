9ef692581335a8232463b6bd1b172c276ea0073e
Florian Steiner <fs@adiwidjaja.com>
2015-08-28 11:09:11 +0200

    Fixed static admincontroller string

--- src/Views/templates/export.blade.php
+++ src/Views/templates/export.blade.php
@@ -1,3 +1,3 @@
 <div class="export">
-	<a href="{{action('AdminController@getExport', ['model' => class_replace($data[0]), 'type' => $data['type']])}}">Export</a>
+	<a href="{{action($controller.'@getExport', ['model' => class_replace($data[0]), 'type' => $data['type']])}}">Export</a>
 </div>
