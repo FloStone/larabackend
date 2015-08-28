6159e087e845f0dc4e9a58cee0b389e976f79892
Florian Steiner <fs@adiwidjaja.com>
2015-08-28 11:11:43 +0200

    Changed version

--- src/Controllers/AdminController.php
+++ src/Controllers/AdminController.php
@@ -40,7 +40,7 @@ abstract class AdminController extends BaseController implements AdminInterface
 {
 	use DispatchesJobs, ValidatesRequests;
 
-	const VERSION = '1.0.5';
+	const VERSION = '1.0.7';
 
 	/**
 	 * Actions displayed in the menu
